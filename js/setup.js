e8dbce8ecaafc21068f87d647b0dbe73ab5dd2cb
Maxim Ryabov <designmr2015@gmail.com>
2018-11-03 11:43:09 +0400

    Задает обработку событий открытия/закрытия попапа и настройки волшебника

--- js/setup.js
+++ js/setup.js
@@ -2,20 +2,69 @@
 
 var NUMBER_OF_OBJECTS = 4;
 
-var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-var SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var ENTER_KEYCODE = 13;
+var ESC_KEYCODE = 27;
+
+var NAMES = [
+  'Иван',
+  'Хуан Себастьян',
+  'Мария',
+  'Кристоф',
+  'Виктор',
+  'Юлия',
+  'Люпита',
+  'Вашингтон'
+];
+var SURNAMES = [
+  'да Марья',
+  'Верон',
+  'Мирабелла',
+  'Вальц',
+  'Онопко',
+  'Топольницкая',
+  'Нионго',
+  'Ирвинг'
+];
+var COAT_COLORS = [
+  'rgb(101, 137, 164)',
+  'rgb(241, 43, 107)',
+  'rgb(146, 100, 161)',
+  'rgb(56, 159, 117)',
+  'rgb(215, 210, 55)',
+  'rgb(0, 0, 0)'
+];
+var EYES_COLORS = [
+  'black',
+  'red',
+  'blue',
+  'yellow',
+  'green'
+];
+var FIREBALL_COLORS = [
+  '#ee4830',
+  '#30a8ee',
+  '#5ce6c0',
+  '#e848d5',
+  '#e6e848'
+];
 
 var userDialog = document.querySelector('.setup');
+
+var coatWizard = userDialog.querySelector('.setup-wizard .wizard-coat');
+var eyesWizard = userDialog.querySelector('.setup-wizard .wizard-eyes');
+var fireball = userDialog.querySelector('.setup-fireball-wrap');
 
 var similarListElement = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template')
   .content
   .querySelector('.setup-similar-item');
 
-userDialog.classList.remove('hidden');
 userDialog.querySelector('.setup-similar').classList.remove('hidden');
+
+var userDialogOpen = document.querySelector('.setup-open');
+var userDialogClose = userDialog.querySelector('.setup-close');
+var userAvatar = userDialogOpen.querySelector('.setup-open-icon');
+var userSettingsSave = userDialog.querySelector('.setup-submit');
 
 /**
  * Генерирует случайное число в диапазоне от min до max (включительно)
@@ -79,3 +128,103 @@ var insertElements = function (parentElement) {
 };
 
 insertElements(similarListElement);
+
+/**
+ * Отправляет данные на сервер
+ * @param  {object} evt - объект event
+ */
+var sendSettingsToServer = function (evt) {
+  evt.submit();
+};
+
+/**
+ * Закрывает попап при нажатии ESC
+ * @param  {object} evt - объект event
+ */
+var popupEscPressHandler = function (evt) {
+  if (evt.which === ESC_KEYCODE) {
+    closePopup();
+  }
+};
+
+/**
+ * Формирует цвет плаща волшебника и записывает значение в соответствующий input (скрытый)
+ */
+var setCoatColor = function () {
+  var newColor = COAT_COLORS[generateRandomNumber(0, COAT_COLORS.length - 1)];
+  userDialog.querySelector('input[name="coat-color"]').value = newColor;
+  coatWizard.style.fill = newColor;
+};
+
+/**
+ * Формирует цвет глаз волшебника и записывает значение в соответствующий input (скрытый)
+ */
+var setEyesColor = function () {
+  var newColor = EYES_COLORS[generateRandomNumber(0, EYES_COLORS.length - 1)];
+  userDialog.querySelector('input[name="eyes-color"]').value = newColor;
+  eyesWizard.style.fill = newColor;
+};
+
+/**
+ * Формирует цвет фаейрболла и записывает значение в соответствующий input (скрытый)
+ */
+var setFireballColor = function () {
+  var newColor = FIREBALL_COLORS[generateRandomNumber(0, FIREBALL_COLORS.length - 1)];
+  userDialog.querySelector('input[name="fireball-color"]').value = newColor;
+  fireball.style.background = newColor;
+};
+
+/**
+ * Открывает попап
+ */
+var openPopup = function () {
+  userDialog.classList.remove('hidden');
+  document.addEventListener('keydown', popupEscPressHandler);
+
+  coatWizard.addEventListener('click', setCoatColor);
+  eyesWizard.addEventListener('click', setEyesColor);
+  fireball.addEventListener('click', setFireballColor);
+
+  userSettingsSave.addEventListener('click', sendSettingsToServer);
+};
+
+/**
+ * Закрывает попап
+ */
+var closePopup = function () {
+  userDialog.classList.add('hidden');
+  document.removeEventListener('keydown', popupEscPressHandler);
+
+  coatWizard.removeEventListener('click', setCoatColor);
+  eyesWizard.removeEventListener('click', setEyesColor);
+  fireball.removeEventListener('click', setFireballColor);
+
+  userSettingsSave.removeEventListener('click', sendSettingsToServer);
+};
+
+/**
+ * Обработчики событий на открытие попапа
+ */
+userDialogOpen.addEventListener('click', function () {
+  openPopup();
+});
+
+userAvatar.addEventListener('keydown', function (evt) {
+  if (evt.which === ENTER_KEYCODE) {
+    openPopup();
+  }
+});
+
+/**
+ * Обработчики событий на закрытие попапа
+ */
+userDialogClose.addEventListener('click', function () {
+  closePopup();
+});
+
+userDialogClose.addEventListener('keydown', function (evt) {
+  if (evt.which === ENTER_KEYCODE) {
+    closePopup();
+  }
+});
+
